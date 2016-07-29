84223da97e330d3c006776930cb6b06ed45aa40d
Lingchao Xin <douglarek@gmail.com>
2016-07-29 18:51:26 +0800

    Func type filter support and cleanups

--- README.md
+++ README.md
@@ -3,5 +3,5 @@
 Auto generate api docs from golang packages based on Iris.
 
 ```
-go run *.go -p .
+go run *.go -p . -t '*iris.Context'
 ```
--- main.go
+++ main.go
@@ -1,43 +1,52 @@
 package main
 
 import (
+	"bytes"
 	"flag"
 	"fmt"
 	"go/doc"
 	"go/parser"
 	"go/token"
 	"go/types"
-	"log"
 )
 
-var path string
+var path, tp string
 
 func init() {
 	flag.StringVar(&path, "p", ".", "path")
+	flag.StringVar(&tp, "t", "string", "type")
 }
 
-func main() {
-	flag.Parse()
-
+func parseDocs(path, tp string) (b []byte, err error) {
 	fset := token.NewFileSet()
 	pkgs, err := parser.ParseDir(fset, path, nil, parser.ParseComments)
 	if err != nil {
-		log.Fatalln(err)
+		return
 	}
+	var buf bytes.Buffer
 	for _, p := range pkgs {
 		p := doc.New(p, ".", doc.AllDecls)
 		for _, f := range p.Funcs {
 			if len(f.Doc) > 0 {
 				ps := f.Decl.Type.Params
 				if ps.NumFields() == 1 {
-					fmt.Println(f.Doc)
 					for _, i := range ps.List {
-						if types.ExprString(i.Type) == "*iris.Context" {
-							fmt.Println(f.Doc) // TODO:
+						if types.ExprString(i.Type) == tp {
+							buf.WriteString(f.Doc)
+							buf.WriteString("\n")
 						}
 					}
 				}
 			}
 		}
 	}
+	buf.Truncate(buf.Len() - 1)
+	return buf.Bytes(), nil
+}
+
+func main() {
+	flag.Parse()
+
+	b, _ := parseDocs(path, tp)
+	fmt.Printf("%s\n", b)
 }
